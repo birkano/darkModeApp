317ad5e7ac46232a580add0e438004b4b2940c33
Birkan <birkanpusa@icloud.com>
2021-12-21 18:10:00 +0300

    manuel light & dark mode ayarı

--- darkModeApp/ViewController.swift
+++ darkModeApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //light mode veya dark mode ayarını manuel yapma
+        overrideUserInterfaceStyle = .dark
     }
 
 
+    //Eğer dark mod & light mod arası geçiş yapıldıysa bu fonksiyon algılar ve fonksiyon içindeki işlemleri yaptırabilirsiniz
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        let userInterfaceStyle = traitCollection.userInterfaceStyle
+        
+        if userInterfaceStyle == .dark {
+            changeButton.tintColor = UIColor.white
+            //mod değişince bilgi mesajı gönderdim
+            let alert = UIAlertController(title: "Bilgi", message: "Dark Mod'a geçtiniz", preferredStyle: UIAlertController.Style.alert)
+                        alert.addAction(UIAlertAction(title: "Tamam", style: UIAlertAction.Style.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+        } else {
+            //mod değşince bilgi mesajı gönderdim
+            let alert = UIAlertController(title: "Bilgi", message: "Light mode'a geçtiniz", preferredStyle: UIAlertController.Style.alert)
+                        alert.addAction(UIAlertAction(title: "Tamam", style: UIAlertAction.Style.default, handler: nil))
+                        self.present(alert, animated: true, completion: nil)
+            changeButton.tintColor = UIColor.red
+        }
+    }
+    
 }
 
